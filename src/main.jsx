f7154d9c123bafbe17b14041b9b45e0b05f85cb4
Heitor Fonseca <heitorffonseca@gmail.com>
2022-03-01 23:31:11 -0300

    insert timer

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Timer from './Timer'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Timer />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
